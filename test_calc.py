e609827b1bb4c206bd9af6a5a0a1eac920669ecc
Demi Fashemo <dfashemo@seas.upenn.edu>
2025-06-30 19:01:24 +0000

    Fixing style issues and working directory path in testing.yaml

--- test_calc.py
+++ test_calc.py
@@ -3,26 +3,26 @@ from calc import Calc
 
 class TestCalc(unittest.TestCase):
 
+
     def setUp(self):
         self.calc = Calc()
 
     def test_add(self):
         self.assertEqual(self.calc.add(2, 3), 5)
-        self.assertEqual(self.calc.add(-3,-2),-5)
+        self.assertEqual(self.calc.add(-3, -2), -5)
 
     def test_sub(self):
         self.assertEqual(self.calc.sub(2, 3), -1)
-        self.assertEqual(self.calc.sub(-5,-3),-2)
+        self.assertEqual(self.calc.sub(-5, -3), -2)
 
     def test_mul(self):
         self.assertEqual(self.calc.mul(2, 3), 6)
 
     def test_div(self):
-        self.assertEqual(self.calc.div(12,3), 4)
+        self.assertEqual(self.calc.div(12, 3), 4)
     
     def test_pow(self):
-        self.assertEqual(self.calc.pow(2,3),8)
+        self.assertEqual(self.calc.pow(2, 3), 8)
 
     def test_sqrt(self):
-        self.assertEqual(self.calc.sqrt(16),4)
-
+        self.assertEqual(self.calc.sqrt(16), 4)
